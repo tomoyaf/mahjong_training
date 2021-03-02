b62e53a1eda7ef847c56e77f0862f2c22ce27ece
tomoyaf <gazimum@gmail.com>
2021-03-03 07:41:31 +0900

    fix design

--- app/components/atoms/card.tsx
+++ app/components/atoms/card.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 import styles from '../../../styles/Card.module.css';
 
-export default function card({ children, className, ...props }) {
-  const cls =
-    className === "" ? styles.card : [styles.card, className].join(" ");
+export default function card({ children, className, ...props }: any) {
+  const cls = className ? styles.card : [styles.card, className].join(" ");
   return (
     <div className={cls} {...props}>
       {children}
